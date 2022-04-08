18a572546f1e02aa6baf48aad2597d1119e1b4e3
kirill <Dom.zavadski@gmail.com>
2022-04-08 17:55:28 +0300

    error

---
+++ src/Testing/admin/admin.js
@@ -0,0 +1,11 @@
+var green_script = document.querySelector(".section3");
+
+function ChangeColor() {
+    if (green_script.checked) {
+        green_script.style.backgroundcolor = "black"
+    } else {
+        alert("Кнопка не нажата")
+    }   
+}
+
+ 
